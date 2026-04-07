a5e9b08753623439b5a66c0d14f37ee276ba90ae
“melissaintrees” <melissaintrees@gmail.com>
2026-04-07 00:12:32 -0700

    Resolving errors

--- build/client/assets/homelayout-DPWF8U8E.js
+++
@@ -1,1 +0,0 @@
-import{w as o,p as t,O as s}from"./chunk-OIYGIGL5-BIqew9Tp.js";const e=()=>t.jsx(t.Fragment,{children:t.jsx(s,{})}),a=o(e);export{a as default};
--- build/client/assets/index-BDvIfddO.js
+++
@@ -1,1 +0,0 @@
-import{w as o,p as e}from"./chunk-OIYGIGL5-BIqew9Tp.js";import{T as r}from"./TwoColumnLayout-DO8Pf6H6.js";import"./proxy-BN7GxTr_.js";const s=()=>e.jsx(r,{h2:"videos",backgroundColor:"bg-darkred",leftChildren:e.jsx("div",{className:"pic",children:"Pic Here"}),rightChildren:e.jsx("div",{className:"text-gray-800",children:"hello"})}),d=o(s);export{d as default};
